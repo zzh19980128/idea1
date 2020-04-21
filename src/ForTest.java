2c12793ecbb9cbbfa427c07a9e01b97afafbfe8a
zzh19980128 <516903127@qq.com>
2020-04-21 15:00:57 +0800

    第一次提交

---
+++ src/ForTest.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ForTest")
+public class ForTest extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
+
---
+++ src/cn/Hello.java
@@ -0,0 +1,11 @@
+package cn;
+
+
+
+public class Hello {
+   public static void main(String[] args) {
+       System.out.println("hello");
+
+    }
+
+}
